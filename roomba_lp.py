1ed0efe2e79ee7d20c316a7795f65325d01bddab
Gabriel Gehrke <g.gehrke@tu-bs.de>
2022-05-01 16:14:56 +0200

    implemented lp roomba, still not working

--- roomba_lp.py
+++ roomba_lp.py
@@ -6,40 +6,37 @@ from random import randrange, random
 from math import sqrt
 import json
 
+"""
+This is an "easier" formulation of the problem, which models the roombas as squares
+and allows to make some optimizations.
+"""
+
 grid_width = 100
 grid_height = 100
 roomba_width = 16
 half_roomba_width = roomba_width//2
 steps = 15
 
-def distance(p1, p2):
-    return sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
-
-
 # Create a new model
 m = gp.Model("Collision-Avoiding Robot Guidance")
 
 class Roomba:
-    def __init__(self, p_start, p_target, speed = 10):
+    def __init__(self, p_start, p_target, speed = 10.0):
         self.p_start = p_start
         self.p_target = p_target
         self.speed = speed
         sqrspeed = self.speed**2
 
-        self.pos_x_vars = [m.addVar(lb=half_roomba_width, ub=grid_width - half_roomba_width, vtype=GRB.CONTINUOUS) for _ in range(steps)]
-        self.pos_y_vars = [m.addVar(lb=half_roomba_width, ub=grid_width - half_roomba_width, vtype=GRB.CONTINUOUS) for _ in range(steps)]
-        self.mov_x_vars = [m.addVar(lb=-self.speed, ub=self.speed, vtype=GRB.CONTINUOUS) for _ in range(steps - 1)]
-        self.mov_y_vars = [m.addVar(lb=-self.speed, ub=self.speed, vtype=GRB.CONTINUOUS) for _ in range(steps - 1)]
+        self.pos_x_vars = m.addMVar(steps, lb = half_roomba_width, ub = grid_width - half_roomba_width, vtype = GRB.CONTINUOUS)
+        self.pos_y_vars = m.addMVar(steps, lb = half_roomba_width, ub = grid_height - half_roomba_width, vtype = GRB.CONTINUOUS)
+        self.mov_x_vars = m.addMVar(steps - 1, lb = -speed, ub = speed, vtype = GRB.CONTINUOUS)
+        self.mov_y_vars = m.addMVar(steps - 1, lb = -speed, ub = speed, vtype = GRB.CONTINUOUS)
 
         # add start and end positions
         m.addConstr(self.pos_x_vars[0] == p_start[0])
         m.addConstr(self.pos_y_vars[0] == p_start[1])
-        m.addConstr(self.pos_x_vars[-1] == p_target[0])
-        m.addConstr(self.pos_y_vars[-1] == p_target[1])
-
-        # limit movement vector:
-        for xmov, ymov in zip(self.mov_x_vars, self.mov_y_vars):
-            m.addConstr(xmov**2 + ymov**2 <= sqrspeed)
+        m.addConstr(self.pos_x_vars[steps - 1] == p_target[0])
+        m.addConstr(self.pos_y_vars[steps - 1] == p_target[1])
 
         # add movement constraints
         for n in range(1, steps):
@@ -50,30 +47,45 @@ class Roomba:
 roombas = list()
 grid_points = [(x, y) for x in range(half_roomba_width, grid_width, roomba_width) for y in range(half_roomba_width, grid_height, roomba_width)]
 
-for _ in range(8):
+for _ in range(2):
     start = grid_points.pop(randrange(0, len(grid_points)))
     target = grid_points.pop(randrange(0, len(grid_points)))
     roombas.append(Roomba(start, target))
 
 # Collision-Avoidance Terms
+non_abses = list()
+abses = list()
 for r1, r2 in combinations(roombas, 2):
     for n in range(steps):
-        dx = r1.pos_x_vars[n] - r2.pos_x_vars[n]
-        dy = r1.pos_y_vars[n] - r2.pos_y_vars[n]
-        min_sqr_dist = roomba_width**2
-        m.addConstr(dx**2 + dy**2 >= min_sqr_dist)
+        # model distance along x and y axis
+        dx = m.addVar(lb = -grid_width, ub = grid_width)
+        dy = m.addVar(lb = -grid_height, ub = grid_height)
+        m.addConstr(dx == r1.pos_x_vars[n] - r2.pos_x_vars[n])
+        m.addConstr(dy == r1.pos_y_vars[n] - r2.pos_y_vars[n])
+        non_abses.append(dx)
+        non_abses.append(dy)
 
-# Define Objective
-#objective = 0
-#for r in roombas:
-#    sqrspeed = r.speed**2
-#    objective += sum(xmov**2 + ymov**2 for xmov, ymov in zip(r.mov_x_vars, r.mov_y_vars))
-#m.setObjective(objective, GRB.MINIMIZE)
+        # model absolute distance
+        absdist_x = m.addVar()
+        absdist_y = m.addVar()
+        abses.append(absdist_x)
+        abses.append(absdist_y)
+        m.addConstr(absdist_x == gp.abs_(dx))
+        m.addConstr(absdist_y == gp.abs_(dy))
+        
+        # avoid collision
+        m.addConstr(absdist_x >= roomba_width)
+        m.addConstr(absdist_y >= roomba_width)
 
 m.setParam('Threads', 12)
-m.setParam('NonConvex', 2)
+#m.setParam('NonConvex', 2)
 m.update()
+
+
 m.optimize()
+if m.status == GRB.INFEASIBLE:
+    print("Infeasible!")
+    exit(1)
 
 
 # get and plot paths
@@ -84,26 +96,24 @@ axes.set_xlim((0, grid_width))
 axes.set_ylim((0, grid_height))
 axes.set_aspect(grid_width / grid_height)
 
-circles = list()
-arrows = list()
 for r in roombas:
     color = tuple(random() for _ in range(3))
     movv = [(r.mov_x_vars[i].x, r.mov_y_vars[i].x) for i in range(steps - 1)]
     posv = [(r.pos_x_vars[i].x, r.pos_y_vars[i].x) for i in range(steps)]
 
     for i, (x, y) in enumerate(posv):
-        # circle
-        circles.append(plt.Circle((x, y), radius=half_roomba_width, color=color, fill=False))
+        # rect
+        axes.add_artist(plt.Rectangle((x - half_roomba_width, y - half_roomba_width), roomba_width, roomba_width, color=color, fill=False))
         # arrow
         if i < steps - 1:
             (dx, dy) = movv[i]
-            arrows.append(plt.arrow(x, y, dx, dy, width=0.1, head_width=2, color=color, shape="full"))
+            plt.arrow(x, y, dx, dy, width=0.1, head_width=2, color=color, shape="full")
 
     paths.append(posv)
-
-for artist in chain(circles, arrows):
-    axes.add_artist(artist)
 
 with open("paths.json", "w") as f:
         json.dump(paths, f)
 plt.savefig("out.png")
+
+print([a.x for a in non_abses])
+print([a.x for a in abses])
